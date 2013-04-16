efbf06c04f77ac74cc0579446658dc140213b4c1
Vanessa <vanessabmdf@gmail.com>
2013-04-15 21:32:07 -0300

    Classe Handebol , com interface pronta

--- ControleAtleta/src/controleatleta/ControleAtleta.java
+++ ControleAtleta/src/controleatleta/ControleAtleta.java
@@ -3,7 +3,7 @@ package controleatleta;
 public class ControleAtleta {
 
     public static void main(String[] args) {
-        CadastroBoxeador cadastro = new CadastroBoxeador();
+        CadastroHandebol cadastro = new CadastroHandebol();
         cadastro.setVisible(true);
     }
 }
